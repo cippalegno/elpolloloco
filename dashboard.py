5eea5bae4b050220654fbe4c51355a335f3498e5
cippalegno <157043115+cippalegno@users.noreply.github.com>
2024-01-19 18:01:42 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -30,8 +30,8 @@ with st.sidebar:
         df = pd.read_excel(filename)
     else:
         #st.warning('Devi caricare un file', icon="⚠️")
-        os.chdir(r"E:\Corso_Python\ElpolloLoco")
-        df = pd.read_excel("test.xlsx")
+        os.chdir(r"C:\ElpolloLoco")
+        df = pd.read_excel("Db.xlsx")
 
 df["Anno"] = df["Data"].dt.year
 df["Mese"] = df["Data"].dt.month
